ba017f30d6bef0833305e2fac5059a11b0e4c2d0
suraj verma <surajverma.off@gmail.com>
2026-07-26 23:28:52 +0530

    the next day

--- if_else/teranaryifelse.java
+++ if_else/teranaryifelse.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class teranaryifelse {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("eneter a positive integer");
+        System.out.print("eneter a positive integer:-");
         int a = sc.nextInt();
          String  c = (a%2==0) ? "even" : "odd ";
         System.out.println(c);
---
+++ loops/printinggm.java
@@ -0,0 +1,12 @@
+package loops;
+
+public class printinggm {
+    public static void main(String[] args) {
+        int a =1 ,d=2;
+        for (int i = 0; i <10 ; i++) {
+            System.out.println(a);
+            a*=2;
+
+        }
+    }
+}
